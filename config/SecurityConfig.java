4bbbb120b24dd259181267fef86f1c778083892c
samilalpay <samilalpay23@gmail.com>
2024-12-16 19:42:48 +0300

    Gerekli Cors ayarları yapıldı

--- config/SecurityConfig.java
+++ config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.samilemir.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -11,8 +10,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.samilemir.handler.AuthEntryPoint;
 import com.samilemir.jwt.JWTAuthenticationFilter;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -24,23 +27,55 @@ public class SecurityConfig {
 
     @Autowired
     private AuthenticationProvider authenticationProvider;
-    
+
+    @Autowired
+    private AuthEntryPoint authEntryPoint;
+
     @Autowired
     private JWTAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(REGISTER, AUTHENTICATE, REFRESH_TOKEN)
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-            )
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authenticationProvider(authenticationProvider)
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http
+                  .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS yapılandırması
+                  .csrf(csrf -> csrf.disable())
+                  .authorizeRequests(auth -> auth
+                      .requestMatchers(REGISTER, AUTHENTICATE, REFRESH_TOKEN).permitAll()
+                      .requestMatchers("/api/v1/airlines/**").authenticated()
+                      .requestMatchers("/api/v1/airport/**").authenticated()
+                      .anyRequest().authenticated()
+                  )
+                  .exceptionHandling(exception -> exception
+                      .authenticationEntryPoint(authEntryPoint)
+                  )
+                  .sessionManagement(session -> session
+                      .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                  )
+                  .authenticationProvider(authenticationProvider)
+                  .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                  .build();
+    }
 
-        return http.build();
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("http://localhost:5173"); // front-end adresinin çalışacağı yer
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    private UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("http://localhost:5173"); // İzin verilen frontend kökeni
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
